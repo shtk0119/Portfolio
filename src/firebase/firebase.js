ce92b2e218237720a39fec792dbbe423ce05a92a
shtk928 <sh.tk928@gmail.com>
2022-10-05 14:17:07 +0900

    firebase link initialize

---
+++ src/firebase/firebase.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDBdrP6TPzXE6SmmIFNIOQsNSiBGCLl5Uc",
+  authDomain: "portfolio-a0aec.firebaseapp.com",
+  projectId: "portfolio-a0aec",
+  storageBucket: "portfolio-a0aec.appspot.com",
+  messagingSenderId: "594371941441",
+  appId: "1:594371941441:web:4f89295f4706ecce13e2c0"
+};
+
+const app = initializeApp(firebaseConfig);
