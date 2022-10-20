db29672056e3f472cdd9f7aab101e1b5e7b23a78
shtk928 <sh.tk928@gmail.com>
2022-10-20 22:18:23 +0900

    TaskGetFunction create

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -4,41 +4,34 @@ import { Add, Delete, FilterList } from '@mui/icons-material';
 import { AddTaskModal } from './AddTaskModal';
 import { db } from '../firebase/firebase';
 import { collection, getDocs, query } from 'firebase/firestore';
-import { getFirestoreData } from '../firebase/getFirestoreData';
 
 type Task = {
   title: string;
   category: string;
   status: '開始前' | '作業中' | '終了';
-  start_date: string; // 型は変更予定
-  end_date: string; // 型は変更予定
+  start_date: string; 
+  end_date: string;
 }
 
 export const TaskList = () => {
   const [isAdd, setIsAdd] = React.useState<boolean>(false);
-  // const [tasks, setTasks] = React.useState<Task[] | undefined>(getFirestoreData);
   const [tasks, setTasks] = React.useState<Task[] | null>(null);
 
   const onClickAddTask = () => {
     setIsAdd(!isAdd);
   } 
 
-  // React.useEffect(() => {
-  //   const q = query(collection(db, 'tasks'));
-  //   const querySnapshot = getDocs(q);
-  //   console.log(querySnapshot);
-  // }, [])
-
   React.useEffect(() => {
     const q = query(collection(db, 'tasks'));
     getDocs(q).then((snapShot) => {
+      // QueryDocumentSnapshot<DocumentData>[] 型の修正
       setTasks(snapShot.docs);
     })
   }, [])
 
   React.useEffect(() => {
-    console.log(tasks)
-  }, [])
+    console.log(tasks?.map(doc => doc.id))
+  }, [tasks])
 
   return (
     <Box
@@ -84,21 +77,22 @@ export const TaskList = () => {
               </ListItem>
               <Divider />
 
-              {/* {tasks?.map((task) => {
+              {tasks?.map((task) => {
                 return (
+                  // 以下コード型修正
                   <Box key={task.id}>
                     <ListItem>
                       <Checkbox />
-                      <ListItemText sx={{ ml: 1, minWidth: '250px', maxWidth: '300px' }}><Link href='#' underline='none'>{task.title}</Link></ListItemText>
-                      <ListItemText sx={{ ml: 1, maxWidth: '280px' }}>{task.category}</ListItemText>
-                      <ListItemText sx={{ maxWidth: '280px' }}>{task.status}</ListItemText>
-                      <ListItemText sx={{ maxWidth: '280px' }}>{task.start_date}</ListItemText>
-                      <ListItemText sx={{ maxWidth: '280px' }}>{task.end_date}</ListItemText>
+                      <ListItemText sx={{ ml: 1, minWidth: '250px', maxWidth: '300px' }}><Link href='#' underline='none'>{task.data().title}</Link></ListItemText>
+                      <ListItemText sx={{ ml: 1, maxWidth: '280px' }}>{task.data().category}</ListItemText>
+                      <ListItemText sx={{ maxWidth: '280px' }}>{task.data().status}</ListItemText>
+                      <ListItemText sx={{ maxWidth: '280px' }}>{task.data().start_date}</ListItemText>
+                      <ListItemText sx={{ maxWidth: '280px' }}>{task.data().end_date}</ListItemText>
                     </ListItem>
                     <Divider /> 
                   </Box>
                 )
-              })} */}
+              })}
             </List>
           </Box>
         </Box>
