5ed058c6088d61de8ab9dc34cc1bfee18f83a7a7
shtk928 <sh.tk928@gmail.com>
2022-10-26 17:39:38 +0900

    checkbox selection release create

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,21 +1,16 @@
 import * as React from 'react';
-import { Box, Checkbox, Divider, IconButton, List, ListItem, ListItemText, Modal, Typography } from '@mui/material';
+import { Box, Checkbox, Divider, IconButton, List, ListItem, ListItemText } from '@mui/material';
 import { Add, Delete, FilterList } from '@mui/icons-material';
 import { AddTaskModal } from './AddTaskModal';
 import { DetailTaskModal } from './DetailTaskModal';
 import { db } from '../firebase/firebase';
 import { collection, deleteDoc, doc, getDocs, onSnapshot, query, QueryDocumentSnapshot } from 'firebase/firestore';
 
-type DeleteTask = {
-  id: string;
-  isCheck: boolean;
-}
-
 export const TaskList = () => {
   const [isAdd, setIsAdd] = React.useState<boolean>(false);
   const [isDetail, setIsDetail] = React.useState<string | null>(null);
   const [tasks, setTasks] = React.useState<QueryDocumentSnapshot[] | null>(null);
-  const [deleteTaskIds, setDeleteTaskIds] = React.useState<DeleteTask[]>([]);
+  const [deleteTaskIds, setDeleteTaskIds] = React.useState<string[]>([]);
 
   const onClickAddTask = () => {
     setIsAdd(!isAdd);
@@ -26,35 +21,28 @@ export const TaskList = () => {
   }
 
   const isCheckedTasks = (e: React.ChangeEvent<HTMLInputElement>, id: string) => {
-    // if を２回に分けている為、コードが冗長になっている修正
-    // if (e.target.checked) {
-    //   setDeleteTaskIds([ ...deleteTaskIds, id ]);
-    // }
+    if (e.target.checked && !deleteTaskIds.includes(id)) {
+      setDeleteTaskIds([ ...deleteTaskIds, id ]);
+    }
 
-    // if (!e.target.checked) {
-    //   setDeleteTaskIds(deleteTaskIds.filter((deleteTaskId) => deleteTaskId !== id));
-    // }
-    
-
+    if (!e.target.checked) {
+      setDeleteTaskIds(deleteTaskIds.filter((deleteTaskId) => deleteTaskId !== id))
+    }
   }
 
   const isCheckedAllTasks = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (tasks && e.target.checked) {
-      setDeleteTaskIds(deleteTaskIds.map((deleteTaskId) => {
-        return {id: deleteTaskId.id, isCheck: true}
-      }));
+      setDeleteTaskIds(tasks?.map((task) => task.id));
     }
 
-    if (tasks && !e.target.checked) {
-      setDeleteTaskIds(deleteTaskIds.map((deleteTaskId) => {
-        return {id: deleteTaskId.id, isCheck: false}
-      }));
+    if (!e.target.checked) {
+      setDeleteTaskIds([]);
     }
   }
 
   const onClickDeleteTask = () => {
     deleteTaskIds.map((deleteTaskId) => {
-      deleteDoc(doc(db, 'tasks', deleteTaskId.id));
+      deleteDoc(doc(db, 'tasks', deleteTaskId));
     });
     setDeleteTaskIds([]);
   }
@@ -63,9 +51,6 @@ export const TaskList = () => {
     const q = query(collection(db, 'tasks'));
     getDocs(q).then((snapShot) => {
       setTasks(snapShot.docs);
-      setDeleteTaskIds(snapShot.docs.map((doc) => {
-        return {id: doc.id, isCheck: false}
-      }));
     });
 
     onSnapshot(q, (snapShot) => {
@@ -73,9 +58,9 @@ export const TaskList = () => {
     });
   }, [])
 
-  // React.useEffect(() => {
-  //   console.log(deleteTaskIds);
-  // }, [deleteTaskIds]);
+  React.useEffect(() => {
+    console.log(deleteTaskIds);
+  }, [deleteTaskIds]);
 
   return (
     <Box
@@ -112,7 +97,7 @@ export const TaskList = () => {
           <Box>
             <List>
               <ListItem>
-                <Checkbox onChange={(e) => isCheckedAllTasks(e)} />
+                <Checkbox checked={tasks?.length === deleteTaskIds.length} onChange={(e) => isCheckedAllTasks(e)} />
                 <ListItemText sx={{ ml: 1, minWidth: '250px', maxWidth: '300px' }}>タイトル</ListItemText>
                 <ListItemText sx={{ ml: 1, maxWidth: '280px' }}>カテゴリー</ListItemText>
                 <ListItemText sx={{ maxWidth: '280px' }}>ステータス</ListItemText>
@@ -125,7 +110,7 @@ export const TaskList = () => {
                 return (
                   <Box key={task.id}>
                     <ListItem>
-                      <Checkbox onChange={(e) => isCheckedTasks(e, task.id)} />
+                      <Checkbox checked={deleteTaskIds.includes(task.id)} onChange={(e) => isCheckedTasks(e, task.id)} />
                       <ListItemText sx={{ ml: 1, minWidth: '250px', maxWidth: '300px', '& .MuiTypography-body1': { display: 'inline', '&:hover': { cursor: 'pointer', opacity: '0.6' } } }} onClick={() => onClickDetailTask(task.id)}>{task.data().title}</ListItemText>
                       <ListItemText sx={{ ml: 1, maxWidth: '280px' }}>{task.data().category}</ListItemText>
                       <ListItemText sx={{ maxWidth: '280px' }}>{task.data().status}</ListItemText>
